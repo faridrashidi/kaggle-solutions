4ed81dd10a1e126563ab26611f44e95c1cd4ba83
Farid Rashidi <farid.rsh@gmail.com>
2021-03-05 20:17:59 -0500

    code update

--- _scripts/extract.py
+++ _scripts/extract.py
@@ -1,29 +1,42 @@
-lastnumber = 403
+lastnumber = 412
+title_id = "cOMZdl"
+desc_id = "iHHIVP"
+subdesc_id = "cUPDWd"
+prize_id = "emXfmI"
+which = '2 months ago'
+
+
 from bs4 import BeautifulSoup
 import sys
-which = '2 months ago'
 with open('url.html', 'r') as f:
     contents = f.read()
     soup = BeautifulSoup(contents, features="html.parser")
-i = 0
-fout = open('new.txt', 'w') # fout = sys.stdout
+i = lastnumber + 1
 for item in soup.find_all("li", {"class": "mdc-list-item"}):
-    date = item.find("span", {"class": "dFGtzH"})
+    date = item.find("span", {"class": subdesc_id})
     if date and date.text.split(' • ')[1] == which:
         i += 1
+fout = open('new.txt', 'w') # fout = sys.stdout
+for item in soup.find_all("li", {"class": "mdc-list-item"}):
+    date = item.find("span", {"class": subdesc_id})
+    if date and date.text.split(' • ')[1] == which:
+        i -= 1
         kind = date.text.split(' • ')[0]
         team = date.text.split(' • ')[-1].replace(' Teams','')
-        team = "{:,}".format(int(team))
+        try:
+            team = "{:,}".format(int(team))
+        except:
+            team = "-"
         link = "https://www.kaggle.com" + item.find("a")["href"]
         image = item.find("img")["src"].split("?t=")[0]
-        year = '2020'
+        year = '2021'
         isHot = 'false'
         done = 'false'
-        title = item.find("div", {"class": "cUUGcu"}).text
-        desc = item.find("span", {"class": "jTyItw"}).text
+        title = item.find("div", {"class": title_id}).text
+        desc = item.find("span", {"class": desc_id}).text
         desc = desc.replace(":", ";").replace("'","")
-        prize = item.find("div", {"class": "ZCsGz"}).text
-        print(f"  - number: '{lastnumber+i}'", file=fout)
+        prize = item.find("div", {"class": prize_id}).text
+        print(f"  - number: '{i}'", file=fout)
         print(f"    title: '{title}'", file=fout)
         print(f"    desc: '{desc}'", file=fout)
         print(f"    kind: '{kind}'", file=fout)
@@ -35,4 +48,8 @@ for item in soup.find_all("li", {"class": "mdc-list-item"}):
         print(f"    isHot: '{isHot}'", file=fout)
         print(f"    done: '{done}'", file=fout)
         print(f"    solutions: ", file=fout)
+        for _ in range(3):
+            print(f"      - rank: ''", file=fout)
+            print(f"        link: ''", file=fout)
+            print(f"        kind: 'description'", file=fout)
 fout.close()
