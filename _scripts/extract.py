bc14addba661ffb9f15269deb03b5d13de326cc1
Farid Rashidi <farid.rsh@gmail.com>
2021-06-25 18:18:57 -0400

    add metric field

--- _scripts/extract.py
+++ _scripts/extract.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
+import yaml
 from kaggle import api
 
-lastnumber = 430
 start = datetime.strptime("2021-04-07", "%Y-%m-%d")
 end = datetime.strptime("2021-06-01", "%Y-%m-%d")
 
 comps = api.competitions_list(page=2)
-i = lastnumber
+with open("../_data/competitions.yml") as fin:
+    config = yaml.load(fin, Loader=yaml.FullLoader)
+i = int(config["competitions"][0]["number"])
 for comp in comps:
     deadline = getattr(comp, "deadline")
     if start.date() < deadline.date() and deadline.date() < end.date():
@@ -16,19 +18,15 @@ fout = open("/Users/farid/Desktop/new.txt", "w")
 for comp in comps:
     deadline = getattr(comp, "deadline")
     if start.date() < deadline.date() and deadline.date() < end.date():
-        # page = requests.get(link)
-        # soup = BeautifulSoup(page.content, "html.parser")
-        # print(soup.find_all("script", {"class": "kaggle-component"})[1])
-
-        # print(comp.evaluationMetric)
         title = comp.title
         desc = comp.description
         desc = desc.replace(":", ";").replace("'", "")
         kind = comp.category
         prize = comp.reward
         team = comp.teamCount
+        metric = comp.evaluationMetric
         link = "https://www.kaggle.com/c/" + comp.ref
-        image = ""
+        image = "https://storage.googleapis.com/kaggle-competitions/kaggle/---/logos/thumb76_76.png"
         year = "2021"
         isHot = "false"
         done = "false"
@@ -38,6 +36,7 @@ for comp in comps:
         print(f"    kind: '{kind}'", file=fout)
         print(f"    prize: '{prize}'", file=fout)
         print(f"    team: '{team}'", file=fout)
+        print(f"    metric: '{metric}'", file=fout)
         print(f"    link: '{link}'", file=fout)
         print(f"    image: '{image}'", file=fout)
         print(f"    year: '{year}'", file=fout)
